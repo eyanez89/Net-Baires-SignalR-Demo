d64fc040f9d54d72005d1f0d9a1d3bd256751327
eyanez <eyanez.89@gmail.com>
2017-08-27 15:21:00 -0300

    hitCounter

---
+++ SignalR-Web/SignalRMVC/Hubs/HitCounterHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalRMVC.Hubs
+{
+    public class HitCounterHub : Hub
+    {
+        static int _count;
+
+        public void RecordHit()
+        {
+            _count += 1;
+
+            Clients.All.receiveHit(_count);
+        }
+        
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalling)
+        {
+            _count -= 1;
+
+            Clients.All.receiveHit(_count);
+
+            return base.OnDisconnected(stopCalling);
+        }
+    }
+}
---
+++ SignalR-Web/SignalRMVC/Scripts/hitCounter.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    // create a signalr connection
+    var con = $.hubConnection();
+
+    // create a hub proxy
+    var hub = con.createHubProxy('hitCounterHub');
+
+    // an event handler for a hub-generated event
+    hub.on('receiveHit', function (count) {
+        $('#counter').text(count);
+    });
+
+    // start the connection
+    con.start(function () {
+
+        // [we're connected now, so] record the site's hit
+        hub.invoke('RecordHit');
+    });
+});
